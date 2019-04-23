6b6fdddcc5a886f9f8b3545fee43da94debbd089
Vicky76 <shekhar804040@gmail.com>
2019-04-23 21:32:25 +0530

    Second Commit

--- ShoppingCart/src/com/Cart/Helper.java
+++ ShoppingCart/src/com/Cart/Helper.java
@@ -7,6 +7,8 @@ public class Helper {
 	private String s1;
 	
 	private String s2;
+	
+	private String s3;
 
 	public String getS1() {
 		return s1;
@@ -22,6 +24,14 @@ public class Helper {
 
 	public void setS2(String s2) {
 		this.s2 = s2;
+	}
+
+	public String getS3() {
+		return s3;
+	}
+
+	public void setS3(String s3) {
+		this.s3 = s3;
 	}
 
 }
