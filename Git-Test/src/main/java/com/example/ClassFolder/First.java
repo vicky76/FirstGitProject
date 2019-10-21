6d8e1ef5f019c94ad2b3a486ffe8ddc649db6192
bivanshu.shekhar <bivanshu.shekhar@globant.com>
2019-10-21 17:50:03 +0530

    Commiting merge

--- Git-Test/src/main/java/com/example/ClassFolder/First.java
+++ Git-Test/src/main/java/com/example/ClassFolder/First.java
@@ -5,6 +5,8 @@ public class First {
 	public void test() {
 		System.out.println("Hello world");
 		System.out.println("Hello Sprint2");
+		System.out.println("Hello Sprint1");
+		System.out.println("Hello master");
 	}
 
 }
